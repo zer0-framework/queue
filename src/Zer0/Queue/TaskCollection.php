1d379fa1f11d322cc3057f78b0c494cd66de0db4
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2019-11-25 13:37:21 +0700

    TaskCollection

---
+++ src/Zer0/Queue/TaskCollection.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Zer0\Queue;
+
+use Zer0\Exceptions\BaseException;
+use Zer0\Exceptions\InvalidStateException;
+use Zer0\Queue\Exceptions\RuntimeException;
+use Zer0\Queue\Pools\Base;
+
+/**
+ * Class TaskCollection
+ * @package Zer0\Queue
+ */
+class TaskCollection
+{
+    /**
+     * @var \SplObjectStorage
+     */
+    protected $pending;
+
+    /**
+     * @var \SplObjectStorage
+     */
+    protected $done;
+
+    /**
+     * @var Base
+     */
+    protected $pool;
+
+    /**
+     * TaskCollection constructor.
+     * @param TaskAbstract ...$args
+     */
+    public function __construct(Base $pool)
+    {
+        $this->pool = $pool;
+        $this->pending = new \SplObjectStorage;
+        $this->done = new \SplObjectStorage;
+    }
+
+    /**
+     * @param TaskAbstract $task
+     * @return $this
+     */
+    public function add(TaskAbstract $task): self
+    {
+        $this->pool->enqueue($task);
+        $this->pending->attach($task);
+        return $this;
+    }
+
+    /**
+     * @param int $seconds
+     * @return $this
+     * @throws Exceptions\WaitTimeoutException
+     */
+    public function wait(int $seconds = 3): self
+    {
+        $this->pool->waitCollection($this, $seconds);
+        return $this;
+    }
+
+    /**
+     * @return \SplObjectStorage
+     */
+    public function pending(): \SplObjectStorage
+    {
+        return $this->pending;
+    }
+
+    /**
+     * @return \SplObjectStorage
+     */
+    public function done(): \SplObjectStorage
+    {
+        return $this->done;
+    }
+}
