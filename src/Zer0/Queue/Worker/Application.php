81c2d4531d60b438f6e2b926ab047001271aaf95
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2020-01-01 09:57:46 +0700

    name

--- src/Zer0/Queue/Worker/Application.php
+++ src/Zer0/Queue/Worker/Application.php
@@ -43,7 +43,7 @@ final class Application extends \PHPDaemon\Core\AppInstance
 
         $this->tasks = new \SplObjectStorage;
 
-        $this->pool = $this->app->factory('QueueAsync');
+        $this->pool = $this->app->factory('QueueAsync', $this->config->name ?? '');
 
         $this->poll();
 
